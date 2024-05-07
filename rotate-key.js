c05f3ee56ab7b27a9044cb88cc836118e0792811
PiVortex <owenhassall@hotmail.co.uk>
2024-05-07 15:50:40 +0100

    added rotate-key

--- rotate-key.js
+++ rotate-key.js
@@ -16,25 +16,31 @@ const connectionConfig = {
   explorerUrl: "https://testnet.nearblocks.io",
 };
 
-async function create_function_access_key(accountId) {
+async function rotate_key(accountId, oldPublicKey) {
     const nearConnection = await connect(connectionConfig); // Connect to NEAR
 
     // Connect to account that will sign the create account transaction
     const account = await nearConnection.account(accountId);
 
     const newKeyPair = KeyPair.fromRandom('ed25519'); // Create key pair
-    const publicKey = newKeyPair.publicKey.toString();
-    const privateKey = newKeyPair.toString();
+    const newPublicKey = newKeyPair.publicKey.toString();
+    const newPrivateKey = newKeyPair.toString();
 
-    await account.addKey(
-        publicKey,
-        "socialmedia.testnet", // Contract with key can call
-        ["post_image", "post_comment"], // Methods on the contract that this key can call
-    );
+    await account.addKey(newPublicKey); // Add key to account
 
     // Print new key pair to console
-    console.log("Public key", publicKey);
-    console.log("Private key", privateKey);
+    console.log("Public key", newPublicKey);
+    console.log("Private key", newPrivateKey);
+
+    // Check if new key exists
+    const keys = await account.getAccessKeys();
+    const exists = keys.some(key => key.public_key === newPublicKey);
+    
+    // If new key exists delete old key
+    if (exists) {
+        await account.deleteKey(oldPublicKey);
+    }
+
 };
 
-create_function_access_key("pivortex.testnet");
+rotate_key("pivortex.testnet", "ed25519:EFS8r2KZ4bAeksSub6F6cbeftVMvaTAbzTAGvTJuqins");
